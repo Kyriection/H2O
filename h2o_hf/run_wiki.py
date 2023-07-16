0402e1bebde818d55a9a92161690e7452cea96a9
Allen <39230030+Kyriection@users.noreply.github.com>
2023-07-16 16:13:30 -0500

    Update run_wiki.py

--- h2o_hf/run_wiki.py
+++ h2o_hf/run_wiki.py
@@ -18,7 +18,7 @@ parser.add_argument("--heavy_ratio", type=float, default=0.1)
 parser.add_argument("--recent_ratio", type=float, default=0.1)
 parser.add_argument("--context_size", type=int, default=512)
 parser.add_argument("--window_size", type=int, default=256)
-parser.add_argument("--num_samples", type=int, default=100)
+parser.add_argument("--num_samples", type=int, default=1000)
 args = parser.parse_args()
 
 
@@ -31,22 +31,22 @@ cache_dir = '../hf_cache'
 
 data = load_dataset(dataset_name, task, split=split)
 
-config = AutoConfig.from_pretrained(model_name_or_path, cache_dir=cache_dir)
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, cache_dir=cache_dir)
-model = AutoModelForCausalLM.from_pretrained(model_name_or_path, cache_dir=cache_dir)
+# config = AutoConfig.from_pretrained(model_name_or_path, cache_dir=cache_dir)
+# tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, cache_dir=cache_dir)
+# model = AutoModelForCausalLM.from_pretrained(model_name_or_path, cache_dir=cache_dir)
 
-if args.heavy_ratio + args.recent_ratio < 1:
-    print('Enable H2O')
-    config.heavy_ratio = args.heavy_ratio
-    config.recent_ratio = args.recent_ratio
-    checkpoint = copy.deepcopy(model.state_dict())
-    model = convert_kvcache_llama_heavy_recent(model, config)
-    model.load_state_dict(checkpoint)
-
-
-model.half().eval().cuda()
+# if args.heavy_ratio + args.recent_ratio < 1:
+#     print('Enable H2O')
+#     config.heavy_ratio = args.heavy_ratio
+#     config.recent_ratio = args.recent_ratio
+#     checkpoint = copy.deepcopy(model.state_dict())
+#     model = convert_kvcache_llama_heavy_recent(model, config)
+#     model.load_state_dict(checkpoint)
 
 
+# model.half().eval().cuda()
+
+import pdb; pdb.set_trace()
 text = "\n\n".join(data["text"][: args.num_samples])
 encodings = tokenizer(text, return_tensors="pt")
 
